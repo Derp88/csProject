adf6b62721018ccf0aa9bb299b5831395746114a
Dylan <62079853+Derp88@users.noreply.github.com>
2021-04-08 14:26:47 -0400

    Added starting image
    added bullets

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ import turtle as trtl
 
 #Variables
 v_gameOver = False
+v_bulletNum = -1
+v_positionCheckerCount = 0
+
+#List
+activeBulletList = []
 
 #Game (WN) screen setup
 wn = trtl.Screen()
@@ -20,18 +25,66 @@ player.color("White")
 player.penup()
 player.goto(0,-300)
 player.setheading(90)
+player.shapesize(5)
 
 #Functions
 def mouseAim(X,Y):
     player.setheading(player.towards(X,Y))
 
 def playerLeft():
-    if player.xcor() > -300:
-        player.setposition(player.xcor()-1, player.ycor())
+    if player.xcor() > -280:
+        player.setposition(player.xcor()-5, player.ycor())
 
 def playerRight():
-    if player.xcor() < 300:
-        player.setposition(player.xcor()+1, player.ycor())
+    if player.xcor() < 280:
+        player.setposition(player.xcor()+5, player.ycor())
+
+def generateBullet(X,Y, HEADING):
+    global v_bulletNum
+
+    #Makes bullet
+    v_bulletNum = v_bulletNum + 1
+    bullet = trtl.Turtle()
+    bullet.penup()
+    bullet.color("White")
+    activeBulletList.append(bullet)
+    
+
+    #Sets bullet
+    activeBulletList[v_bulletNum].setx(X)
+    activeBulletList[v_bulletNum].sety(Y)
+    activeBulletList[v_bulletNum].setheading(HEADING)
+    wn.update()
+
+def moveBullet():
+    wn.update()
+    global v_bulletNum
+    global BulletX
+    global BulletY
+    global BulletHeading
+    global v_positionCheckerCount
+    
+
+    for activeBullet in activeBulletList:
+        v_positionCheckerCount = v_positionCheckerCount + 1
+        #Reports bullet current position to collision checker
+        if v_positionCheckerCount % 100 == 0:
+            BulletX = activeBullet.xcor()
+            BulletY = activeBullet.ycor()
+            BulletHeading = activeBullet.heading()
+        
+        activeBullet.sety(activeBullet.ycor()-.1)
+
+        #This removes the bullet when it goes out of bounds
+        if activeBullet.xcor() > 300 or activeBullet.xcor() < -300 or activeBullet.ycor() > 300 or activeBullet.ycor() < -300:
+            activeBullet.hideturtle()
+            activeBulletList.remove(activeBullet)
+            v_bulletNum = v_bulletNum - 1
+
+def mainPerodic():
+    moveBullet()
+
+generateBullet(0,0,270)
 
 #Inputs
 wn.onscreenclick(mouseAim)
@@ -39,8 +92,12 @@ wn.onkeypress(playerLeft, "a")
 wn.onkeypress(playerRight, "d")
 wn.listen()
 
-while True:
+#Game Loop
+while v_gameOver == False:
     wn.update()
+    mainPerodic()
+
+    
 
 
 
